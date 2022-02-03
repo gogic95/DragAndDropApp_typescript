6867320f3da03b1ad25a129182fa1cd303bd3ab2
gogic95 <filipgogic95@gmail.com>
2022-02-03 18:11:13 +0100

    app.js compiled

--- dist/app.js
+++ dist/app.js
@@ -46,37 +46,3 @@ __decorate([
     Autobind
 ], ProjectInput.prototype, "submitHandler", null);
 const projectInput = new ProjectInput();
-// console.log('Radi');
-// const projectInputTemplate = document.getElementById('project-input') as HTMLTemplateElement;
-// document.body.appendChild(projectInputTemplate.content.cloneNode(true));
-// const projectListTemplate = document.getElementById('project-list') as HTMLTemplateElement;
-// document.body.appendChild(projectListTemplate.content.cloneNode(true));
-// class project {
-//     constructor(public title: string, public description: string, public people: number) {
-//     }
-// }
-// let projects: project[] = [];
-// function ubaciUListu() {
-//     const titleEl = document.getElementById('title') as HTMLInputElement;
-//     let title = titleEl.value;
-//     const descriptionEl = document.getElementById('description') as HTMLTextAreaElement;
-//     let description = descriptionEl.value;
-//     const peopleEl = document.getElementById('people') as HTMLInputElement;
-//     let people = +peopleEl.value;
-//     if (title != '' && description != '' && people > 0) {
-//         let p = new project(title, description, people);
-//         projects.push(p);
-//         const li = document.createElement("li");
-//         li.appendChild(document.createTextNode(`Project: ${title}; Description: ${description}; No. of people: ${people}
-//         `));
-//         ul.appendChild(li);
-//     }
-//     else {
-//         alert("Invalid input!");
-//         return;
-//     }
-// }
-// const button = document.getElementsByTagName('button')[0];
-// const ul = document.getElementsByTagName('ul')[0];
-// button.addEventListener('click', ubaciUListu);
-// console.log(projects);
